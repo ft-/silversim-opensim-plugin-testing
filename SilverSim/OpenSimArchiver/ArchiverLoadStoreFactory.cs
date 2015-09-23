19c483c3e13aabcf59682f978aeb3247f8ebd38c
ft <ft@noemail>
2015-09-23 18:06:23 +0200

    finalized new OpenSimArchiver plugin

---
+++ SilverSim/OpenSimArchiver/ArchiverLoadStoreFactory.cs
@@ -0,0 +1,20 @@
+﻿// SilverSim is distributed under the terms of the
+// GNU Affero General Public License v3
+
+using SilverSim.Main.Common;
+
+namespace SilverSim.OpenSimArchiver
+{
+    [PluginName("OpenSimArchiveSupport")]
+    public class ArchiverLoadStoreFactory : IPluginFactory
+    {
+        public ArchiverLoadStoreFactory()
+        {
+
+        }
+        public IPlugin Initialize(ConfigurationLoader loader, Nini.Config.IConfig ownSection)
+        {
+            return new ArchiverLoadStore();
+        }
+    }
+}
---
+++ SilverSim/OpenSimArchiver/Common/AssetExtensionMethods.cs
@@ -0,0 +1,32 @@
+﻿// SilverSim is distributed under the terms of the
+// GNU Affero General Public License v3
+
+using SilverSim.Main.Common.Tar;
+using SilverSim.Types;
+using SilverSim.Types.Asset;
+using System.IO;
+
+namespace SilverSim.OpenSimArchiver.Common
+{
+    public static class AssetExtensionMethods
+    {
+        public static AssetData LoadAsset(
+            this TarArchiveReader reader,
+            TarArchiveReader.Header hdr,
+            UUI creator)
+        {
+            AssetData asset = new AssetData();
+            asset.FileName = hdr.FileName;
+            byte[] assetData = new byte[hdr.Length];
+            if(hdr.Length != reader.Read(assetData, 0, hdr.Length))
+            {
+                throw new IOException();
+            }
+            asset.Name = "From Archive";
+            asset.Data = assetData;
+            asset.Creator = creator;
+
+            return asset;
+        }
+    }
+}
