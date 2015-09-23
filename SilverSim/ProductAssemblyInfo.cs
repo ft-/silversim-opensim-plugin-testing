99946ce0f40b8cff84265380e97c212219a9379c
ft <ft@noemail>
2015-09-23 17:32:11 +0200

    moved OpenSim specific connectors and handlers into silversim-opensim-plugin-dev

---
+++ SilverSim/ProductAssemblyInfo.cs
@@ -0,0 +1,15 @@
+// SilverSim is distributed under the terms of the
+// GNU Affero General Public License v3
+
+using System.Reflection;
+
+[assembly: AssemblyProduct("SilverSim/Arriba")]
+[assembly: AssemblyCompany("SilverSim Development")]
+[assembly: AssemblyCopyright("Affero GPLv3 License")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: AssemblyVersion("0.0.0.0")]
+[assembly: AssemblyFileVersion("0.0.0.0")]
+
+[assembly: SilverSim.Types.Assembly.InterfaceVersion("0.0.0.0")]
---
+++ runprebuild.sh
@@ -0,0 +1,31 @@
+#!/bin/sh
+
+case "$1" in
+
+  'clean')
+
+    mono Prebuild/Prebuild.exe /clean
+
+  ;;
+
+
+  'autoclean')
+
+    echo y|mono Prebuild/Prebuild.exe /clean
+
+  ;;
+
+
+  'vs2010')
+  
+    mono Prebuild/Prebuild.exe /target vs2010
+  
+  ;;
+
+  *)
+
+    mono Prebuild/Prebuild.exe /target vs2010
+
+  ;;
+
+esac
