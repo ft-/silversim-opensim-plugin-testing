05e5fb165c383f4391724f319cd7537ecc25cad2
ft <ft@noemail>
2016-12-25 02:00:07 +0100

    code cleanup

--- SilverSim/BackendHandlers.Robust/Maptile/MaptileServerHandler.cs
+++ SilverSim/BackendHandlers.Robust/Maptile/MaptileServerHandler.cs
@@ -42,7 +42,7 @@ namespace SilverSim.BackendHandlers.Robust.Maptile
             }
             catch(ConfigurationLoader.ServiceNotFoundException)
             {
-
+                /* intentionally ignored */
             }
         }
 
