15f7ab265bd516901bde457bc59c5c02aefe3389
ft <ft@noemail>
2017-01-01 20:32:04 +0100

    added groups stub servers

---
+++ SilverSim/BackendHandlers.Robust/Groups/BaseGroupsServerHandler.cs
@@ -0,0 +1,201 @@
+﻿// SilverSim is distributed under the terms of the
+// GNU Affero General Public License v3
+
+using Nini.Config;
+using SilverSim.Main.Common;
+using SilverSim.Main.Common.HttpServer;
+using SilverSim.Scene.Management.IM;
+using SilverSim.ServiceInterfaces.Groups;
+using SilverSim.Types;
+using SilverSim.Types.Asset;
+using SilverSim.Types.Groups;
+using SilverSim.Types.StructuredData.REST;
+using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Xml;
+
+namespace SilverSim.BackendHandlers.Robust.Groups
+{
+    public abstract class BaseGroupsServerHandler : IPlugin
+    {
+        protected GroupsServiceInterface m_GroupsService { get; private set; }
+        readonly string m_GroupsServiceName;
+        protected readonly Dictionary<string, Action<HttpRequest, Dictionary<string, object>>> MethodHandlers = new Dictionary<string, Action<HttpRequest, Dictionary<string, object>>>();
+        protected BaseHttpServer m_HttpServer { get; private set; }
+        protected IMRouter m_IMRouter { get; private set; }
+        protected abstract string UrlPath { get; }
+
+        internal BaseGroupsServerHandler(IConfig ownSection)
+        {
+            m_GroupsServiceName = ownSection.GetString("GroupsService", "GroupsService");
+        }
+
+        public virtual void Startup(ConfigurationLoader loader)
+        {
+            MethodHandlers.Add("ADDNOTICE", HandleAddNotice);
+            MethodHandlers.Add("GETROLEMEMBERS", HandleGetRoleMembers);
+            MethodHandlers.Add("GETROLES", HandleGetRoles);
+            MethodHandlers.Add("GETMEMBERS", HandleGetMembers);
+            m_GroupsService = loader.GetService<GroupsServiceInterface>(m_GroupsServiceName);
+            m_HttpServer = loader.HttpServer;
+            m_HttpServer.UriHandlers.Add(UrlPath, GroupsHandler);
+            try
+            {
+                loader.HttpsServer.UriHandlers.Add(UrlPath, GroupsHandler);
+            }
+            catch
+            {
+                /* intentionally left empty */
+            }
+        }
+
+        void GroupsHandler(HttpRequest req)
+        {
+            if (req.ContainsHeader("X-SecondLife-Shard"))
+            {
+                req.ErrorResponse(HttpStatusCode.MethodNotAllowed, "Request source not allowed");
+                return;
+            }
+
+            if (req.Method != "POST")
+            {
+                req.ErrorResponse(HttpStatusCode.MethodNotAllowed);
+                return;
+            }
+
+            Dictionary<string, object> data;
+            try
+            {
+                data = REST.ParseREST(req.Body);
+            }
+            catch
+            {
+                req.ErrorResponse(HttpStatusCode.BadRequest);
+                return;
+            }
+
+            if (!data.ContainsKey("METHOD"))
+            {
+                req.ErrorResponse(HttpStatusCode.BadRequest);
+                return;
+            }
+        }
+
+        protected static bool String2Boolean(string s)
+        {
+            int i;
+            if(s.ToLower() == "true")
+            {
+                return true;
+            }
+            else if(s.ToLower() == "false")
+            {
+                return false;
+            }
+            else if(int.TryParse(s, out i))
+            {
+                return i != 0;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        protected virtual void HandleGetRoleMembers(HttpRequest req, Dictionary<string, object> reqdata)
+        {
+
+        }
+
+        protected virtual void HandleGetRoles(HttpRequest req, Dictionary<string, object> reqdata)
+        {
+
+        }
+
+        protected virtual void HandleGetMembers(HttpRequest req, Dictionary<string, object> reqdata)
+        {
+
+        }
+
+        void HandleAddNotice(HttpRequest req, Dictionary<string, object> reqdata)
+        {
+            /*
+            RequestingAgentID =
+            GroupID =
+            NoticeID =
+            FromName =
+            Subject =
+            Message =
+            HasAttachment =
+
+            optionally
+            AttachmentType =
+            AttachmentName =
+            AttachmentItemID =
+            AttachmentOwnerID =
+            */
+            UUI requestingAgent = UUI.Unknown;
+            GroupNotice notice = new GroupNotice();
+            try
+            {
+                requestingAgent = new UUI(reqdata["RequestingAgentID"].ToString());
+                notice.Group.ID = reqdata["GroupID"].ToString();
+                notice.ID = reqdata["NoticeID"].ToString();
+                notice.FromName = reqdata["FromName"].ToString();
+                notice.Subject = reqdata["Subject"].ToString();
+                notice.Message = reqdata["Message"].ToString();
+                notice.HasAttachment = String2Boolean(reqdata["HasAttachment"].ToString());
+                if(notice.HasAttachment)
+                {
+                    notice.AttachmentType = (AssetType)int.Parse(reqdata["AttachmentType"].ToString());
+                    notice.AttachmentName = reqdata["AttachmentName"].ToString();
+                    notice.AttachmentItemID = reqdata["AttachmentItemID"].ToString();
+                    notice.AttachmentOwner.ID = reqdata["AttachmentOwnerID"].ToString();
+                }
+            }
+            catch
+            {
+                req.ErrorResponse(HttpStatusCode.BadRequest);
+            }
+
+            try
+            {
+                m_GroupsService.Notices.Add(requestingAgent, notice);
+            }
+            catch
+            {
+                SendBooleanResponse(req, false);
+                return;
+            }
+            SendBooleanResponse(req, true);
+        }
+
+        protected void SendBooleanResponse(HttpRequest req, bool result, string msg = "")
+        {
+            using (HttpResponse res = req.BeginResponse("text/xml"))
+            {
+                using (XmlTextWriter writer = res.GetOutputStream().UTF8XmlTextWriter())
+                {
+                    writer.WriteStartElement("ServerResponse");
+                    writer.WriteNamedValue("RESULT", result ? "true" : "false");
+                    writer.WriteEndElement();
+                }
+            }
+        }
+
+        protected void SendNullResult(HttpRequest req, string msg)
+        {
+            using (HttpResponse res = req.BeginResponse("text/xml"))
+            {
+                using (XmlTextWriter writer = res.GetOutputStream().UTF8XmlTextWriter())
+                {
+                    writer.WriteStartElement("ServerResponse");
+                    writer.WriteNamedValue("RESULT", "NULL");
+                    writer.WriteNamedValue("REASON", msg);
+                    writer.WriteEndElement();
+                }
+            }
+        }
+    }
+}
---
+++ SilverSim/BackendHandlers.Robust/Groups/HGGroupsServerHandler.cs
@@ -0,0 +1,250 @@
+﻿// SilverSim is distributed under the terms of the
+// GNU Affero General Public License v3
+
+using log4net;
+using Nini.Config;
+using SilverSim.Main.Common;
+using SilverSim.Main.Common.HttpServer;
+using SilverSim.ServiceInterfaces;
+using SilverSim.Types;
+using SilverSim.Types.Groups;
+using SilverSim.Types.IM;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Net;
+
+namespace SilverSim.BackendHandlers.Robust.Groups
+{
+    [Description("Robust HG-Groups Protocol Server")]
+    public class RobustHGGroupsServerHandler : BaseGroupsServerHandler, IServiceURLsGetInterface
+    {
+        static readonly ILog m_Log = LogManager.GetLogger("ROBUST HGGROUPS HANDLER");
+        public RobustHGGroupsServerHandler(IConfig ownSection)
+            : base(ownSection)
+        {
+            MethodHandlers.Add("POSTGROUP", HandlePostGroup);
+        }
+
+        protected override string UrlPath
+        {
+            get
+            {
+                return "/hg-groups";
+            }
+        }
+
+        public void GetServiceURLs(Dictionary<string, string> dict)
+        {
+            dict["GroupsServerURI"] = m_HttpServer.ServerURI;
+        }
+
+        public override void Startup(ConfigurationLoader loader)
+        {
+            m_Log.Info("Initializing handler for HG-Groups server");
+            base.Startup(loader);
+        }
+
+        void HandlePostGroup(HttpRequest req, Dictionary<string, object> reqdata)
+        {
+            GroupInfo grec_new = new GroupInfo();
+            GroupInvite ginvite = new GroupInvite();
+            UUI agent;
+            UUI requestingAgent;
+            string accessToken;
+            try
+            {
+                grec_new.ID.ID = reqdata["GroupID"].ToString();
+                grec_new.ID.GroupName = reqdata["Name"].ToString();
+                grec_new.ID.HomeURI = new Uri(reqdata["Location"].ToString(), UriKind.Absolute);
+                accessToken = reqdata["AccessToken"].ToString();
+                agent = new UUI(reqdata["AgentID"].ToString());
+                requestingAgent = new UUI(reqdata["RequestingAgentID"].ToString());
+            }
+            catch
+            {
+                req.ErrorResponse(HttpStatusCode.BadRequest);
+                return;
+            }
+
+            GroupInfo grec;
+            if(!m_GroupsService.Groups.TryGetValue(requestingAgent, grec_new.ID, out grec))
+            {
+                try
+                {
+                    m_GroupsService.Groups.Update(requestingAgent, grec_new);
+                }
+                catch(Exception e)
+                {
+                    SendBooleanResponse(req, false, e.Message);
+                    return;
+                }
+                grec = grec_new;
+            }
+
+            if(grec.ID.HomeURI == null || grec.ID.HomeURI.ToString() != grec_new.ID.HomeURI.ToString())
+            {
+                SendBooleanResponse(req, false, "Cannot create proxy membership for a non-proxy group");
+                return;
+            }
+
+            ginvite.ID = UUID.Random;
+            ginvite.Group = grec.ID;
+            ginvite.RoleID = UUID.Zero;
+            ginvite.Principal = agent;
+            try
+            {
+                m_GroupsService.Invites.Add(requestingAgent, ginvite);
+            }
+            catch(Exception e)
+            {
+                SendBooleanResponse(req, false, e.Message);
+                return;
+            }
+
+            GridInstantMessage gim = new GridInstantMessage();
+            gim.FromGroup = grec.ID;
+            gim.ToAgent = agent;
+            gim.FromAgent = requestingAgent;
+            gim.Dialog = GridInstantMessageDialog.GroupInvitation;
+            gim.IsFromGroup = true;
+            gim.Message = "Please confirm your acceptance to join group " + grec.ID.FullName;
+
+            if(!m_IMRouter.SendSync(gim))
+            {
+                SendBooleanResponse(req, false);
+                return;
+            }
+
+            
+            try
+            {
+                m_GroupsService.Members.Add(requestingAgent, grec.ID, agent, UUID.Zero, accessToken);
+            }
+            catch(Exception e)
+            {
+                SendBooleanResponse(req, false, e.Message);
+                return;
+            }
+
+            SendBooleanResponse(req, true);
+        }
+
+        bool VerifyAccessToken(UUI requestingAgent, UGI group, string accessToken)
+        {
+            GroupMember gmem;
+            try
+            {
+                if(!m_GroupsService.Members.TryGetValue(requestingAgent, group, requestingAgent, out gmem))
+                {
+                    return false;
+                }
+            }
+            catch
+            {
+                return false;
+            }
+            return gmem.AccessToken == accessToken;
+        }
+
+        void HandleGetGroup(HttpRequest req, Dictionary<string, object> reqdata)
+        {
+            if(!reqdata.ContainsKey("GroupID") && !reqdata.ContainsKey("Name"))
+            {
+                req.ErrorResponse(HttpStatusCode.BadRequest);
+                return;
+            }
+        }
+
+        protected override void HandleGetRoleMembers(HttpRequest req, Dictionary<string, object> reqdata)
+        {
+            UUI requestingAgent = UUI.Unknown;
+            UGI group = UGI.Unknown;
+            string accessToken = string.Empty;
+            try
+            {
+                requestingAgent = new UUI(reqdata["RequestingAgentID"].ToString());
+                group.ID = reqdata["GroupID"].ToString();
+                accessToken = reqdata["AccessToken"].ToString();
+            }
+            catch
+            {
+                req.ErrorResponse(HttpStatusCode.BadRequest);
+                return;
+            }
+            if(!VerifyAccessToken(requestingAgent, group, accessToken))
+            {
+                SendNullResult(req, string.Empty);
+                return;
+            }
+
+            base.HandleGetRoleMembers(req, reqdata);
+        }
+
+        protected override void HandleGetRoles(HttpRequest req, Dictionary<string, object> reqdata)
+        {
+            UUI requestingAgent = UUI.Unknown;
+            UGI group = UGI.Unknown;
+            string accessToken = string.Empty;
+            try
+            {
+                requestingAgent = new UUI(reqdata["RequestingAgentID"].ToString());
+                group.ID = reqdata["GroupID"].ToString();
+                accessToken = reqdata["AccessToken"].ToString();
+            }
+            catch
+            {
+                req.ErrorResponse(HttpStatusCode.BadRequest);
+                return;
+            }
+            if (!VerifyAccessToken(requestingAgent, group, accessToken))
+            {
+                SendNullResult(req, string.Empty);
+                return;
+            }
+
+            base.HandleGetRoles(req, reqdata);
+        }
+
+        protected override void HandleGetMembers(HttpRequest req, Dictionary<string, object> reqdata)
+        {
+            UUI requestingAgent = UUI.Unknown;
+            UGI group = UGI.Unknown;
+            string accessToken = string.Empty;
+            try
+            {
+                requestingAgent = new UUI(reqdata["RequestingAgentID"].ToString());
+                group.ID = reqdata["GroupID"].ToString();
+                accessToken = reqdata["AccessToken"].ToString();
+            }
+            catch
+            {
+                req.ErrorResponse(HttpStatusCode.BadRequest);
+                return;
+            }
+            if (!VerifyAccessToken(requestingAgent, group, accessToken))
+            {
+                SendNullResult(req, string.Empty);
+                return;
+            }
+
+            base.HandleGetMembers(req, reqdata);
+        }
+    }
+
+    #region Factory
+    [PluginName("HGGroupsHandler")]
+    public sealed class RobustHGGroupsServerHandlerFactory : IPluginFactory
+    {
+        public RobustHGGroupsServerHandlerFactory()
+        {
+
+        }
+
+        public IPlugin Initialize(ConfigurationLoader loader, IConfig ownSection)
+        {
+            return new RobustHGGroupsServerHandler(ownSection);
+        }
+    }
+    #endregion
+}
